269625eecd000d42c97a96784754b39d1fa1551c
“khalid <khalid.sabbah@yahoo.com>
2024-12-30 14:22:19 +0200

    first commit

---
+++ .history/vite.config_20241230141911.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/my-vite-app/', // Replace 'my-vite-app' with your repository name
+});
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/stablo/', // Replace 'my-vite-app' with your repository name
+});
